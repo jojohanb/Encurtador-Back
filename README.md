d1ccc8dc0e75098d8babe4213ded8104bc784143
jojohanb <randesungasabonete@gmail.com>
2025-10-18 20:05:26 -0300

    não sei se ta indo

---
+++ README.md
@@ -0,0 +1,1 @@
+Adicionar as anotações do projeto aqui ! ! !
---
+++ teste-db.js
@@ -0,0 +1,15 @@
+import db from "./src/infra/connection.js";
+import { users } from "./src/infra/db/schema.js"; 
+
+async function testConnection() {
+  try {
+    // Apenas para ver se o banco responde
+    const result = await db.select().from(users).limit(1);
+    console.log("Conexão com Drizzle funcionando!");
+    console.log(result);
+  } catch (error) {
+    console.error(" Erro ao testar Drizzle:", error);
+  }
+}
+
+testConnection();
